e6da18e0de97be98b0b186551d313abf0a0cd2c0
SatyamSharma64 <satyamkailashsharma@gmail.com>
2023-03-07 11:39:46 +0530

    deployed

--- client/src/scenes/widgets/AdvertWidget.jsx
+++ client/src/scenes/widgets/AdvertWidget.jsx
@@ -20,7 +20,7 @@ const AdvertWidget = () => {
         width="100%"
         height="auto"
         alt="advert"
-        src="http://localhost:3001/assets/Travel.jpg"
+        src="https://interact.onrender.com/assets/Travel.jpg"
         style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
       />
       <FlexBetween>
